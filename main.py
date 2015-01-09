9fe52451fe9652635e8f2846c4c353792f8e7549
syjsmk <syjsmk@naver.com>
2015-01-09 17:15:47 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+__author__ = 'user'
+
+import http.client
+import os
+import urllib.parse
+# conn = http.client.HTTPConnection("localhost", 8080)
+# conn.request("GET", "/api/worker")
+
+branchName = os.environ.get('TRAVIS_BRANCH')
+repoSlug = os.environ.get('TRAVIS_REPO_SLUG')
+
+print(os.environ.get('TRAVIS_BRANCH'))
+print(os.environ.get('TRAVIS_REPO_SLUG'))
+
+# print('branchName : ' + branchName)
+# print('repoSlug : ' + repoSlug)
+
+header = {
+   'User-Agent': 'MyClient/1.0.0',
+   'Accept': 'application/vnd.travis-ci.2+json',
+   'Host': 'api.travis-ci.org'
+}
+conn = http.client.HTTPSConnection('api.travis-ci.org')
+conn.request("GET", "/", headers=header)
+
+
+
+response = conn.getresponse()
+print('response : ', response.status, response.reason)
+data = response.read()
+print('data : ', data)
